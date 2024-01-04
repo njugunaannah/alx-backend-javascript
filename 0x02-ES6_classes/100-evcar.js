933ebf8ddaa9790496eaddd1c11a78ed809e41b3
“Annah <njugunaannah@gmail.com>
2024-01-04 22:43:26 +0300

    evcar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,41 +1,13 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-const _range = Symbol('range');
+/* eslint-disable */
+import Car from './10-car';
 
-class Car {
-  constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
-  }
+export default class EVCar extends Car {
+	constructor(brand, motor, color, range) {
+		super(brand, motor, color);
+		this._range = range;
+	}
 
-  cloneCar() {
-    const { [_brand]: brand, [_motor]: motor, [_color]: color } = this;
-    return new Car(brand, motor, color);
-  }
-
-  toString() {
-    return `Car { _brand: ${this[_brand]}, _motor: ${this[_motor]}, _color: ${this[_color]} }`;
-  }
+	cloneCar() {
+		return new Car();
+	}
 }
-
-class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this[_range] = range;
-  }
-
-  cloneCar() {
-    const {
-      [_brand]: brand, [_motor]: motor, [_color]: color, [_range]: range,
-    } = this;
-    return new Car(brand, motor, color, range);
-  }
-
-  toString() {
-    return `EVCar { _brand: ${this[_brand]}, _motor: ${this[_motor]}, _color: ${this[_color]}, _range: ${this[_range]} }`;
-  }
-}
-
-export default EVCar;
